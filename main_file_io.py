068f0e64cb8e77a568419a7bbe36b69abc1e6f3b
ivanranet <ivanranet@yandex.ru>
2024-11-22 21:40:30 +0500

    Добавил обработку исключений

--- main_file_io.py
+++ main_file_io.py
@@ -1,27 +1,33 @@
-from locale import windows_locale
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
-from PyInstaller.loader.pyiboot01_bootstrap import entry
-from Scripts.bottle import response
 
 
 def upload():
-    file_path = fd.askopenfilename()
-    if file_path:
-        files = {'file': open(file_path, 'rb')}
-        response = requests.post('httrs://file.io', files = files)
-        if response.status_code == 200:
-            link = response.json()['link']
-            entry.insert(0, link)
+    try:
+        file_path = fd.askopenfilename()
+        if file_path:
+            with open(file_path, 'rb') as f:
+                files = {'file': f}
+                response = requests.post('https://file.io', files = files)
+                response.raise_for_status()
+                link = response.json()['link']
+                entry.delete(0, END)
+                entry.insert(0, link)
+    except Exception as e:
+        mb.showerror('Ошибка!', f'Произошла ошибка: {e}')
 
 
 window = Tk()
-window.title('Сохранение файла в облаке')
+window.title('Сохранение файлов в облаке')
 window.geometry('400x200')
 
-button = ttk.Button('Загрузить файл', command = upload)
+button = ttk.Button(text = 'Загрузить файл', command = upload)
 button.pack()
 
-entry = ttk.Entry.pack()
+entry = ttk.Entry()
+entry.pack()
+
+window.mainloop()
